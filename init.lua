613aa8c694061106ea0a3108d100cd194029709c
carametal <mkatsdevelop@gmail.com>
2025-04-18 22:45:04 +0900

    全画面表示/左半分/右半分を実装

--- init.lua
+++ init.lua
@@ -3,6 +3,27 @@ hs.loadSpoon("EmmyLua")
 local hotkey = require("hs.hotkey")
 local window = require("hs.window")
 local alert = require("hs.alert")
+local layout = require("hs.layout")
+
+local function moveToUnit(rect)
+	local win = window.focusedWindow()
+	if win then
+		win:moveToUnit(rect)
+	else
+		alert.show("No focused Window")
+	end
+end
+
+local function isWindowMaximized()
+	local win = window.focusedWindow()
+	if win then
+		local frame = win:frame()
+		local screen = win:screen()
+		return frame == screen:fromUnitRect(hs.layout.maximized)
+	else
+		alert.show("No focused Window")
+	end
+end
 
 hotkey.bind({ "cmd", "alt", "ctrl", "shift" }, "r", function()
 	hs.reload()
@@ -14,5 +35,17 @@ hotkey.bind({ "cmd", "alt", "ctrl" }, "f", function()
 		win:maximize()
 	else
 		alert.show("No focused window")
+	end
+end)
+
+hotkey.bind({ "cmd", "alt", "ctrl", "shift" }, "left", function()
+	if isWindowMaximized() then
+		moveToUnit(layout.left50)
+	end
+end)
+
+hotkey.bind({ "cmd", "alt", "ctrl", "shift" }, "right", function()
+	if isWindowMaximized() then
+		moveToUnit(layout.right50)
 	end
 end)
